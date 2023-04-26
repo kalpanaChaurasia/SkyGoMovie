8d39994827122895cbf94c6838d08fa397df918f
kalpana.c <kalpana.c@tatelxsi.co.in>
2023-04-26 18:40:51 +0530

    Added search functionality- User can filter movies by searching for terms in the title or by the genre

--- app/src/main/java/com/sky/skygomovie/ui/movie/MovieActivity.kt
+++ app/src/main/java/com/sky/skygomovie/ui/movie/MovieActivity.kt
@@ -1,8 +1,11 @@
 package com.sky.skygomovie.ui.movie
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.GridLayoutManager
 import com.sky.skygomovie.R
 import com.sky.skygomovie.data.Movies
@@ -21,11 +24,36 @@ class MovieActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMovieBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        movieAdapter = MovieAdapter()
+
         //setup action bar
+
         //add search
+
         //add observer
         observeMovieData()
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.search_menu, menu)
+        val searchItem: MenuItem = menu.findItem(R.id.actionSearch)
+        val searchView: SearchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            android.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                movieAdapter.filter.filter(query)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                movieAdapter.filter.filter(newText)
+                return false
+            }
+        })
+        return true
     }
 
     private fun observeMovieData() {
@@ -43,9 +71,8 @@ class MovieActivity : AppCompatActivity() {
     }
 
     private fun handleSuccessState(data: Movies?) {
-        println("data?.size  ${data?.size}")
         data?.let {
-            movieAdapter = MovieAdapter(it)
+            movieAdapter.addMovieData(it)
             binding.rvMovies.apply {
                 layoutManager = GridLayoutManager(
                     applicationContext,
@@ -65,5 +92,6 @@ class MovieActivity : AppCompatActivity() {
 
 
     }
+
 
 }
--- app/src/main/java/com/sky/skygomovie/ui/movie/MovieAdapter.kt
+++ app/src/main/java/com/sky/skygomovie/ui/movie/MovieAdapter.kt
@@ -2,15 +2,34 @@ package com.sky.skygomovie.ui.movie
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.sky.skygomovie.data.Movies
+import com.sky.skygomovie.data.MoviesItem
 import com.sky.skygomovie.databinding.ItemMovieBinding
+import java.util.*
 
-class MovieAdapter(private val movies: Movies) : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+class MovieAdapter() : RecyclerView.Adapter<MovieAdapter.ViewHolder>(),
+    Filterable {
+    private var mMoviesData: ArrayList<MoviesItem> = ArrayList()
+    private var mDataFiltered: ArrayList<MoviesItem> = ArrayList()
 
-    class ViewHolder(val binding: ItemMovieBinding) :
-        RecyclerView.ViewHolder(binding.root) {}
+    fun addMovieData(list: ArrayList<MoviesItem>) {
+        mMoviesData = list
+        mDataFiltered = mMoviesData
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(val binding: ItemMovieBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(moviesItem: MoviesItem) {
+            Glide.with(binding.ivMovie.context)
+                .load(moviesItem.Poster)
+                .into(binding.ivMovie)
+
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -23,12 +42,42 @@ class MovieAdapter(private val movies: Movies) : RecyclerView.Adapter<MovieAdapt
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        Glide.with(holder.itemView)
-            .load(movies[position].Poster)
-            .into(holder.binding.ivMovie)
+        holder.bind(mDataFiltered!![position])
+
     }
 
     override fun getItemCount(): Int {
-        return movies.size
+        return mDataFiltered?.size ?: 0
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+
+                val charString = constraint?.toString() ?: ""
+                mDataFiltered = if (charString.isEmpty()) {
+                    mMoviesData
+                } else {
+                    val filteredList = ArrayList<MoviesItem>()
+                    mMoviesData.filter {
+                        (it.Title.lowercase(Locale.getDefault())
+                            .contains(charString.lowercase(Locale.getDefault())))
+                                || (it.Genre.lowercase(Locale.getDefault())
+                            .contains(charString.lowercase(Locale.getDefault())))
+
+                    }.forEach { filteredList.add(it) }
+                    filteredList
+                }
+                return FilterResults().apply { values = mDataFiltered }
+            }
+
+            override fun publishResults(charSequence: CharSequence, filterResults: FilterResults) {
+                mDataFiltered = if (filterResults.values == null)
+                    ArrayList()
+                else
+                    filterResults.values as ArrayList<MoviesItem>
+                notifyDataSetChanged()
+            }
+        }
     }
 }
